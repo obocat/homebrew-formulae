d9bd6be4ae33858034b3d48c283873931f58644a
Daniel Toranzo <daniel@obocat.com>
2023-11-30 15:30:33 +0100

    Update xcode-xlsx.rb

--- Formula/xcode-xlsx.rb
+++ Formula/xcode-xlsx.rb
@@ -1,8 +1,8 @@
 class XcodeXlsx < Formula
   desc "xcode-xlsx is a command line tool for importing translations from a .xlsx file to iOS strings resources .strings as simple key/value pairs accordingly."
   homepage "https://github.com/obocat/xcode-xlsx"
-  url "https://github.com/obocat/xcode-xlsx.git", tag: "0.0.1"
-  version "0.0.1"
+  url "https://github.com/obocat/xcode-xlsx.git", tag: "1.0.0"
+  version "1.0.0"
   license "MIT"
 
   def install
